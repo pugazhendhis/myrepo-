743032de1b5357699fea13d56ddbc046f27153e1
pugazhendhi <pugazhendhimca@gmail.com>
2015-11-23 13:25:31 +0530

    my first push

---
+++ Gittest/src/pugazh/Sample.java
@@ -0,0 +1,14 @@
+package pugazh;
+
+public class Sample 
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		//second commit
+		System.out.println("First change i made");
+
+	}
+
+
+}
